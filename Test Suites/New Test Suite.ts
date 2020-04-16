f3ad993822d9601c46dda405ee80f69ef5119900
Safa Fekih Hassen <safafekihhassen@gmail.com>
2020-04-16 10:40:08 +0100

    message

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0ef08f71-6f05-4764-99ec-05a362d9184d</testSuiteGuid>
+   <testCaseLink>
+      <guid>f522ffef-b128-478e-acfe-1ac3822acd9c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6dfef876-0139-4f45-9479-906caead0b19</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case (1)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e35e3048-325f-4e16-8fd4-32c8ead0386a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testButton</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
